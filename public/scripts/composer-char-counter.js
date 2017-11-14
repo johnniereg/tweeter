289ffced5482b3b5227a9576ccb7bde4a4863094
Johnnie Regalado <johnniereg@gmail.com>
2017-11-14 19:28:31 +0000

    Variables now point to and grab text input and total characters entered for composer-char-counter function.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,25 @@
 console.log("Script loaded.");
 
+// function updateCharCount() {
+//   let maxChar = 140;
+//   let
+// }
+
 $(document).ready(function() {
 
   console.log("Document Ready.");
 
-  $(".new-tweet").on("input", function() {
+  let textField = $("textarea[name='text']");
+
+  $(textField).on("input", function() {
     console.log("Input received.");
+
+    let inputText = textField.val();
+    let totalCharsTyped = inputText.length;
+
+
+    console.log(inputText);
+    console.log(totalCharsTyped);
   });
 
 });
