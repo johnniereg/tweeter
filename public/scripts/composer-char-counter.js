34316d03c0f1bf5113f9bfc539fabcc5b64bc310
Johnnie Regalado <johnniereg@gmail.com>
2017-11-14 19:56:31 +0000

    Fully implemented character counter. Created new CSS selector for negative class which gets added to the counter when the remaining characters drops below 0 and turns it red. Will reset to black if user deletes characters.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,25 +1,24 @@
-console.log("Script loaded.");
-
-// function updateCharCount() {
-//   let maxChar = 140;
-//   let
-// }
-
 $(document).ready(function() {
-
-  console.log("Document Ready.");
-
-  let textField = $("textarea[name='text']");
-
+  // Targets the text area of the form.
+  let textField = $(".new-tweet").find("textarea[name='text']");
+  // When the text area receives any input.
   $(textField).on("input", function() {
-    console.log("Input received.");
+    // Sets total typed characters.
+    let totalCharsTyped = $(this).val().length;
+    // Sets remaining characters.
+    let remainingChars = 140 - totalCharsTyped;
+    // Selects the counter in this form.
+    let charCounter = $(this).closest(".new-tweet").find(".counter");
 
-    let inputText = textField.val();
-    let totalCharsTyped = inputText.length;
-
-
-    console.log(inputText);
-    console.log(totalCharsTyped);
+    // Replaces counter text with remaining counter amount.
+    charCounter.text(remainingChars);
+    // Adds class of "negative" in order to colour counter red.
+    if (remainingChars < 0) {
+      charCounter.addClass("negative");
+    }
+    // If user brings tweet back into character limit, remove negative class.
+    if (remainingChars >= 0) {
+      charCounter.removeClass("negative");
+    }
   });
-
 });
