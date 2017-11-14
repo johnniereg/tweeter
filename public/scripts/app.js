75e343e361616335e12e3187988de80e301898f0
Johnnie Regalado <johnniereg@gmail.com>
2017-11-14 22:40:08 +0000

    Added placeholder info for testing createTweetElement fn

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test code for function.
+var tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
 
- function createTweetElement() {
+function createTweetElement(tweetdata) {
 
- }
+}
+
+
+
+let $tweet = createTweetElement(tweetData);
+
+// console.log($tweet); // to see what it looks like
+// $('#tweets').append($tweet); // to add to page.
