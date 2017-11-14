42640eff8810bf5f9c98a1ba3b04b22462fff7ef
Johnnie Regalado <johnniereg@gmail.com>
2017-11-14 18:17:20 +0000

    Verified script loading and that .ready is working too.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,8 @@
+console.log("Script loaded.");
+$(document).ready(function() {
+  console.log("Document Ready.");
+  // $(this).on('click', function() {
+  //   console.log("Clicked.");
+  // });
+
+});
