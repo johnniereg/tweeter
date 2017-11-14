a2fff8586048468f82e1b328eb4fc487975d9944
Johnnie Regalado <johnniereg@gmail.com>
2017-11-14 21:04:55 +0000

    Refactored character counter function to use more specificity in targetting and also gave a different class name for the error state of no characters remaining.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,22 +2,20 @@ $(document).ready(function() {
   // Targets the text area of the form.
   let textField = $(".new-tweet").find("textarea[name='text']");
   // When the text area receives any input.
-  $(textField).on("input", function() {
+  textField.on("input", function() {
     // Sets total typed characters.
     let totalCharsTyped = $(this).val().length;
     // Sets remaining characters.
     let remainingChars = 140 - totalCharsTyped;
     // Selects the counter in this form.
-    let charCounter = $(this).closest(".new-tweet").find(".counter");
+    let charCounter = $(this).nextAll(".counter");
     // Replaces counter text with remaining counter amount.
     charCounter.text(remainingChars);
-    // Adds class of "negative" in order to colour counter red.
+    // If they run out of characters.
     if (remainingChars < 0) {
-      charCounter.addClass("negative");
-    }
-    // If user brings tweet back into character limit, remove negative class.
-    if (remainingChars >= 0) {
-      charCounter.removeClass("negative");
+      charCounter.addClass("error");
+    } else {
+      charCounter.removeClass("error");
     }
   });
 });
