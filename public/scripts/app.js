141acb5d441e039cb6fc8aec8300aab7ed611151
Johnnie Regalado <johnniereg@gmail.com>
2017-11-15 01:45:13 +0000

    Removed placeholder tweet from index. Adjusted opacity of non-hovered tweets to 0.9 and implemeneted function renderTweets to add tweet elements to the page.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,22 +5,52 @@
  */
 
 // Test code for function.
-var tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-};
-
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
 // Builds the element for a unique tweet to add to HTML.
 function createTweetElement(tweet) {
@@ -48,12 +78,19 @@ function createTweetElement(tweet) {
   return theTweet;
 }
 
-let $tweet = createTweetElement(tweetData);
+// Given a database of tweets, render them on the index page.
+function renderTweets(tweets) {
+  // Loops through tweet and creates element for each one.
+  tweets.forEach(function(tweet) {
+    let tweetElement = createTweetElement(tweet);
+    // Adds the tweet element to the container.
+    $('#tweets').append(tweetElement);
+  });
+}
 
-console.log($tweet); // to see what it looks like
-
+// Render tweets.
 $(document).ready(function() {
-  $('#tweets').append($tweet); // to add to page.
+  renderTweets(data);
 });
 
 
