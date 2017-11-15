8f74349ea6c383fa7ab2345198609d519e2af8d3
Johnnie Regalado <johnniereg@gmail.com>
2017-11-15 03:07:41 +0000

    Refactored code and changed variable name theTweet to tweetElement to be more semantic.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -54,37 +54,41 @@ var data = [
 
 // Builds the element for a unique tweet to add to HTML.
 function createTweetElement(tweet) {
+
   // Build the header element.
   let $header = $("<header>")
-                  .append(`<img class="avatar" src="${tweet.user.avatars.small}">`)
-                  .append(`<div class="name">${tweet.user.name}</div>`)
-                  .append(`<div class="handle">${tweet.user.handle}</div>`);
+    .append(`<img class="avatar" src="${tweet.user.avatars.small}">`)
+    .append(`<div class="name">${tweet.user.name}</div>`)
+    .append(`<div class="handle">${tweet.user.handle}</div>`);
+
   // Build the tweet section.
   let $section = $("<section>")
-                  .append("<div>").text(tweet.content.text).addClass("tweet-body");
+    .append("<div>").text(tweet.content.text).addClass("tweet-body");
+
   // Determine how many days ago the tweet was created.
-  let daysAgo = Math.floor((Date.now() - tweet.created_at) / (1000*60*60*24));
+  let daysAgo = Math.floor((Date.now() - tweet.created_at) / (1000 * 60 * 60 * 24));
+
   // Build the footer section.
   let $footer = $("<footer>")
-                  .append(`<div class="tweet-date">${daysAgo} days ago</div>`)
-                  .append("<i class='fa fa-heart'>")
-                  .append("<i class='fa fa-retweet'>")
-                  .append("<i class='fa fa-flag'>");
+    .append(`<div class="tweet-date">${daysAgo} days ago</div>`)
+    .append("<i class='fa fa-heart'>")
+    .append("<i class='fa fa-retweet'>")
+    .append("<i class='fa fa-flag'>");
+
   // Compile header, section, footer into article element.
-  let theTweet = $("<article>").addClass("tweet")
-                             .append($header)
-                             .append($section)
-                             .append($footer);
-  return theTweet;
+  let tweetElement = $("<article class='tweet'>")
+    .append($header)
+    .append($section)
+    .append($footer);
+  return tweetElement;
 }
 
 // Given a database of tweets, render them on the index page.
 function renderTweets(tweets) {
   // Loops through tweet and creates element for each one.
   tweets.forEach(function(tweet) {
-    let tweetElement = createTweetElement(tweet);
     // Adds the tweet element to the container.
-    $('#tweets').append(tweetElement);
+    $('#tweets').append(createTweetElement(tweet));
   });
 }
 
