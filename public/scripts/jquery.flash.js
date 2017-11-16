d4ecc969fe0307060e44f630e25a37c176c2fcf7
Johnnie Regalado <johnniereg@gmail.com>
2017-11-16 21:04:54 +0000

    Removed instructional comments.

--- public/scripts/jquery.flash.js
+++ public/scripts/jquery.flash.js
@@ -1,30 +1,14 @@
-// 
+//
 // jQuery Flash plugin v1.0
-// 
+//
 //  - Created by Joel Moss at Codaset (joel@codaset.com)
 //  - http://codaset.com/codaset/jquery-flash
-// 
-// 
-// Simply call the following to show a flash message with the text "This is my message":
-// 
-//    $.flash('This is my message');
-// 
-// Or you can call it on an element, where the flash message will be populated from the
-// contents of the element:
-// 
-//    $('#my-element').flash();
-// 
-// 
-// To install, just include this javascript file and the accompanying CSS file into your
-// HTML page. And that's it!
-// 
-// 
-// If you need help, found a bug, or would like to contribute, head on over to
-// http://codaset.com/codaset/jquery-flash
-// 
+//
+// Used to handle flash messages when tweets are empty or too long.
+//
 
-(function($){ 
-  
+(function($){
+
   $(function() {
 
     var flashDiv = '<div id="flash" style="display:none;"> \
@@ -32,11 +16,11 @@
                       <div class="flash_inner"></div> \
                     </div>';
     $(flashDiv).appendTo('body');
-  
+
     $.extend({
       flash: function(content, options) {
         var flash = $('#flash');
-      
+
         flash.find('.flash_inner').html(content);
         flash
           .show()
@@ -46,7 +30,7 @@
             top: (($(window).height()-flash.height()))-50+'px',
             opacity: 0.8
           });
-        
+
         $(document).click(function(){
           flash.animate({
             top: ($(window).height()+50)+'px',
@@ -55,7 +39,7 @@
         });
       }
     });
-  
+
     $.fn.extend({
       bottom: function() {
         var offLeft = Math.floor((($(window).width()-this.width())/2)-20),
@@ -66,7 +50,7 @@
         });
         return this;
       },
-    
+
       flash: function(options) {
         $.flash(this.html(), options);
         return this;
@@ -74,5 +58,5 @@
     });
 
   });
-  
-})(jQuery); 
+
+})(jQuery);
