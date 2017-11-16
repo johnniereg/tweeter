0509d746270f1ee8915e4990cc04276c8d33ba55
Johnnie Regalado <johnniereg@gmail.com>
2017-11-16 21:03:37 +0000

    Cleaned up code, adjusted commenting.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   // Targets the text area of the form.
   let textField = $(".new-tweet").find("textarea[name='text']");
-  // When the text area receives any input.
+
   textField.on("input", function() {
-    // Sets total typed characters.
+    // Counts typed characters and sets remaining characters.
     let totalCharsTyped = $(this).val().length;
-    // Sets remaining characters.
     let remainingChars = 140 - totalCharsTyped;
-    // Selects the counter in this form.
-    let charCounter = $(this).nextAll(".counter");
+
     // Replaces counter text with remaining counter amount.
+    let charCounter = $(this).nextAll(".counter");
     charCounter.text(remainingChars);
-    // If they run out of characters.
+
+    // If they run out of characters sets error class.
     if (remainingChars < 0) {
       charCounter.addClass("error");
     } else {
