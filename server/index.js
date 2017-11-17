c6e051cfa79974afeed88129d18dc887753ba0f9
Johnnie Regalado <johnniereg@gmail.com>
2017-11-17 19:40:12 +0000

    Set-up sass middleware

--- server/index.js
+++ server/index.js
@@ -2,12 +2,21 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT            = 8080;
+const express         = require("express");
+const bodyParser      = require("body-parser");
+const sassMiddleware  = require("node-sass-middleware");
+const path            = require("path");
+const app             = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(sassMiddleware({
+  src: "./public",
+  debug: true,
+  outputStyle: "compressed",
+}));
+
 app.use(express.static("public"));
 
 // Define MongoDB connection parameters.
